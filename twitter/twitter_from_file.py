11899a34d61c3d946d0063bbbe92b303a55cefeb
Ajeet Singh Raina, Docker Captain, ARM Innovator <ajeetraina@gmail.com>
2023-06-15 16:50:50 +0530

    Update twitter_from_file.py

--- twitter/twitter_from_file.py
+++ twitter/twitter_from_file.py
@@ -1,3 +1,12 @@
+# In this example, the code reads the Twitter data from a JSON file called twitter_data.json. 
+# Each tweet in the JSON file is processed individually, and the sentiment analysis is performed using TextBlob. 
+# The sentiment polarity is calculated, and based on its value, a sentiment label (Positive, Negative, or Neutral) is assigned. 
+# The tweet text and sentiment label are then printed.
+
+Make sure to replace 'twitter_data.json' with the actual path to your JSON file containing the Twitter data.
+
+By storing the Twitter data in a JSON file, you can easily process large amounts of data and perform sentiment analysis on each tweet. This approach allows you to decouple the data storage from the sentiment analysis code and provides flexibility in handling different data sources.
+
 import json
 from textblob import TextBlob
 
